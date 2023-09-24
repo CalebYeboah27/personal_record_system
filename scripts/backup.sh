5dfcb3971011ef0727a626b058927a36caf45cfe
CalebYeboah27 <calebyeboah27@gmail.com>
2023-09-24 18:43:32 -0400

    Added a random password generator

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "Hello world"
+
+DIR="/home/cyeboah/personal_record_system"
+
+SOURCE_DIR="$DIR/notes"
+BACKUP_DIR="$DIR/backups"
+
+BACKUP_FOLDER="$BACKUP_DIR/$(date +\%Y-\%m-\%d)"
+
+rsync -av --delete "$SOURCE_DIR/" "$BACKUP_FOLDER/"
+
+crontab -e
+
+exit 1
